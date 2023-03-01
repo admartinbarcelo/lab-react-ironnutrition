6f6b50418469c915978b4d5c2ccc7796933a3900
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-03-01 17:52:03 +0100

    retocado el delete

--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,29 +1,26 @@
 import { Card, Col, Button } from 'antd';
 
-export function FoodBox({food , onDelete}){
-    const { name, image, calories, servings } = food;
-    const totalCalories = parseInt(calories) * parseInt(servings);
+export function FoodBox({ food, onDelete }) {
+  const { name, image, calories, servings } = food;
+  const totalCalories = parseInt(calories) * parseInt(servings);
 
-    const handleDelete = () => {
-      onDelete(name);
-    }
-    
+  const handleDelete = () => {
+    onDelete(name);
+  };
 
-
-    return (
-        <Col>
-          <Card
-            title={name}
-            style={{ width: 230, height: 300, margin: 10 }}
-          >
-            <img src={image} height={60} alt="food" />
-            <p>Calories: {calories}</p>
-            <p>Servings: {servings}</p>
-            <p>
-              <b>Total Calories: {totalCalories} </b> kcal
-            </p>
-            <Button type="primary" onClick={handleDelete}> Delete </Button>
-          </Card>
-        </Col>
-      );
+  return (
+    <Col>
+      <Card title={name} style={{ width: 230, height: 300, margin: 10 }}>
+        <img src={image} height={60} alt="food" />
+        <p>Calories: {calories}</p>
+        <p>Servings: {servings}</p>
+        <p>
+          <b>Total Calories: {totalCalories} </b> kcal
+        </p>
+        <Button type="primary" onClick={handleDelete}>
+          Delete
+        </Button>
+      </Card>
+    </Col>
+  );
 }
